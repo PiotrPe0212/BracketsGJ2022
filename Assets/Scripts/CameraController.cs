c738c1ba0f9832e5c12dfe08d2fa6d5b50456eab
Piotr Pietraszek <pietraszek.dev@gmail.com>
2022-02-21 21:05:32 +0100

    camera controller gamplay controller
    initial state

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+
+    [SerializeField] private float timeOffset;
+    [SerializeField] private  Vector2 posOffset;
+
+    private GameObject player;
+    Vector3 initialPos = new Vector3(-8f, 5f, -1f);
+    private bool correctInitPos;
+ 
+    // Update is called once per frame
+    void LateUpdate()
     {
-        
+        player = GameObject.Find("player");
+        if (player)
+        {
+            Vector3 startPos = transform.position;
+            Vector3 endPos = player.transform.position;
+            endPos.x += posOffset.x;
+            endPos.y -= posOffset.y;
+            endPos.z = -1;
+
+            transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+            correctInitPos = false;
+        }
+        else
+        {
+            if (!correctInitPos)
+                InitialCamera();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitialCamera()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, initialPos, timeOffset * Time.deltaTime);
+        correctInitPos = true;
     }
 }
--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 initPos = new Vector3(0, 0, 0);
+    [SerializeField] private int InitMentalHealth = 10;
+    private bool gameStarted = false;
+    public int ActualMentalHealth;
+
+
+    private void Awake()
+    {
+        GameManager.OnGameStateChange += OnStateChangeMenuChange;
+    }
+    private void OnDestroy()
+    {
+        GameManager.OnGameStateChange -= OnStateChangeMenuChange;
+    }
+    // Start is called before the first frame update
+    private void OnStateChangeMenuChange(GameManager.GameState newState)
+    {
+        if(newState == GameManager.GameState.PlayGame)
+        {
+            InitFunction();
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +37,19 @@ public class GameplayManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+    }
+
+    private void InitFunction()
+    {
+        gameStarted = true;
+        ActualMentalHealth = InitMentalHealth;
+        Instantiate(player, initPos, Quaternion.identity);
+
+        //move to the parrent gameObject
+
+
     }
 }
